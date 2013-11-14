b1ec25ea67f5f97aeb97f1fb35718723351ee33c
Jeff Ericson <jaericson@gmail.com>
2013-11-14 00:52:28 +0000

    initial commit

---
+++ proxy_server/views.py
@@ -0,0 +1,9 @@
+# Create your views here.
+
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt
+def home(request):
+    print(request.POST)
+    return HttpResponse('{"correct": true, "score": 1, "msg": "<p>Great! You got the right answer!</p>"}')
