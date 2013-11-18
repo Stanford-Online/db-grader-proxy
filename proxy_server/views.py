904c47b7f5d37dcbf3fd791d3f31f9f9f3a581a5
Jeff <jaericson@gmail.com>
2013-11-18 03:25:33 -0800

    Trying real feedback 3

--- proxy_server/views.py
+++ proxy_server/views.py
@@ -41,6 +41,7 @@ def home(request):
         ipdb.set_trace()
 
         feedback = graded.get('feedback')[0].get('explanation', '<p>No Explanation</p>').strip().encode('ascii', 'ignore')
+        feedback.replace("\"", "'")
 
         # Needs to be enclosed within <p>...</p> tags and <br> must be <br/>
         # feedback = '<p><br/><font style="color:green; font-weight:bold;">Correct</font><br/><br/>Your Query Result: <table border="1" style="font-size:90%; padding: 1px;border-spacing: 0px; border-collapse: separate"><tr><td>E.T.</td></tr><tr><td>Raiders of the Lost Ark</td></tr></table><br/>Expected Query Result: <table border="1" style="font-size:90%; padding: 1px;border-spacing: 0px; border-collapse: separate"><tr><td>E.T.</td></tr><tr><td>Raiders of the Lost Ark</td></tr></table></p>'
