52cc7387d3b48dd545e00e719cccad91f25f0558
Jeff <jaericson@gmail.com>
2013-11-18 02:10:56 -0800

    Using request.body

--- proxy_server/views.py
+++ proxy_server/views.py
@@ -6,4 +6,6 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def home(request):
     print(request.POST)
+    print("\n---\n")
+    print(request.body)
     return HttpResponse('{"correct": true, "score": 1, "msg": "<p>Great! You got the right answer!</p>"}')
