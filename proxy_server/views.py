68d2e8f2d30b5fc684c3b7ac202d280a24698d41
Jeff <jaericson@gmail.com>
2013-11-19 14:11:45 -0800

    Added breakpoint

--- proxy_server/views.py
+++ proxy_server/views.py
@@ -43,10 +43,11 @@ def home(request):
         maxScore = str(graded.get('maximum-score', 1))
         isCorrect = "true" if score == maxScore else "false"
 
-        # import ipdb
-        # ipdb.set_trace()
+        import ipdb
+        ipdb.set_trace()
 
         # Consider re instead of a bunch of calls to replace function
+        # Alternatively, could change PHP code to correct returned XML/HTML
         feedback = graded.get('feedback')[0].get('explanation', '<p>No Explanation</p>').strip().encode('ascii', 'ignore')
         feedback = "<p>" + feedback.replace("\"", "'").replace("<br>", "<br/>").replace("\n", "<br/>") + "</p>"
 
