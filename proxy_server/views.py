3db7223cdefdfa6141785953f9d10cebe05a63d0
Jeff <jaericson@gmail.com>
2013-12-07 17:43:33 -0800

    Testing quotes

--- proxy_server/views.py
+++ proxy_server/views.py
@@ -46,11 +46,11 @@ def home(request):
         # Consider re instead of a bunch of calls to replace function
         # Alternatively, could change PHP code to correct returned XML/HTML
         feedback = graded.get('feedback')[0].get('explanation', '<p>No Explanation</p>').strip().encode('ascii', 'ignore')
-        # feedback = "<p>" + feedback.replace("\"", "'").replace("<br>", "<br/>").replace("\n", "<br/>").replace("<pre/>", "<pre>") + "</p>"
-        feedback = "<p>" + feedback.replace("<br>", "<br/>").replace("\n", "<br/>").replace("<pre/>", "<pre>") + "</p>"
+        feedback = "<p>" + feedback.replace("\"", "'").replace("<br>", "<br/>").replace("\n", "<br/>").replace("<pre/>", "<pre>") + "</p>"
+        # feedback = "<p>" + feedback.replace("<br>", "<br/>").replace("\n", "<br/>").replace("<pre/>", "<pre>") + "</p>"
 
-    # return HttpResponse('{"correct": ' + isCorrect + ', "score": ' + score + ', "msg": "' + feedback + '"}')
-    return HttpResponse("{\"correct\": " + isCorrect + ", \"score\": " + score + ", \"msg\": ''" + feedback + "'}")
+    return HttpResponse('{"correct": ' + isCorrect + ', "score": ' + score + ', "msg": "' + feedback + '"}')
+    # return HttpResponse("{\"correct\": " + isCorrect + ", \"score\": " + score + ", \"msg\": ''" + feedback + "'}")
 
 
 # TODO: Change "Queued..." in UI to something like "Checking"
